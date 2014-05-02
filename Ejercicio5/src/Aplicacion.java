24c005fef438633558f43de93351257e00a0fe8c
jausinez <WORK@JUsuario>
2014-05-02 14:49:29 +0200

    Segunda versión

--- Ejercicio5/src/Aplicacion.java
+++ Ejercicio5/src/Aplicacion.java
@@ -13,8 +13,9 @@ public class Aplicacion {
 	//Constructor
 	Aplicacion() {
 		ventana = new Ventana();
-		//Definir ventana
+		//Definir ventana	
 		ventana.setSize(ventana.getContentPane().getWidth() + 20, ventana.getContentPane().getHeight() + 40);
+		ventana.setTitle("Tres en raya");
 		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		PosicionVentana.centrarVentana(ventana);
 		ventana.setVisible(true);
